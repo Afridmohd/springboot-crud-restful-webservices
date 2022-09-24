e9aba1a7a36b2c78850b0ade3f1b4bf2a4023700
Afrid <afridmohammed1822@gmail.com>
2022-09-25 00:44:57 +0530

    First Commit

---
+++ src/test/java/net/javaguides/springboorcrudrestfulwebservices/SpringboorCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboorcrudrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboorCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
